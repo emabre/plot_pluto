701a91238bb069f6681e2b1a55e722eca39c6f5d
Momo <emabr90@gmail.com>
2018-11-21 18:48:23 +0100

    I added a function to read vtk log files (vtk.out)

--- pluto_read_frm.py
+++ pluto_read_frm.py
@@ -67,9 +67,20 @@ def pluto_vtk_to_numpy(filename, quantity_names, ordering):
 
     return u, x, y, z
 
-def pluto_read_vtk_frame(pluto_dir, pluto_nframe, q_names=None):
+def pluto_read_vtk_frame(pluto_dir, n=None, t=None, q_names=None):
     '''Reads a vtk dataframe which was written by pluto 4.2, only in non-vector mode.
-    Returns a dictionary with the quantities and ND arrays of x,y,z positions'''
+    Returns a dictionary with the quantities, ND arrays of x,y,z positions and an int or a float
+    containig the actual time and number of frame'''
+
+    # if n!=None and t==None:
+    #     pluto_nframe = n
+    #     pluto_time = leggi il tempo del frame n
+    # elif n==None and t!=None:
+    #     trovare l'n che più si avvicina a t
+    #     pluto_nframe = qualcosa
+    #     pluto_time
+    # else
+    #     raise ValueError("Epecify either n or t, not both or none.")
 
     # Names of the quantities (if not feeded as input)
     if q_names==None:
@@ -86,8 +97,35 @@ def pluto_read_vtk_frame(pluto_dir, pluto_nframe, q_names=None):
     u, x, y, z = pluto_vtk_to_numpy(vtk_fi, q_names, ordering)
     # Build the dictionary
     q = {q_names[ii]:u[ii] for ii in range(len(q_names))}
-    return q, x, y, z
 
+    return q, x, y, z, pluto_time, pluto_nframe
+
+def read_vtk_log(log_fi):
+    '''Function to read pluto's vtk log files (vtk.out)'''
+
+    with open(log_fi) as log:
+        lines = log.readlines()
+
+    nvtk = []; t = []; dt = []; nsteps = []
+    file_type = []; endianess = []
+    quantity_names = []
+    for line in lines:
+        elements = line.strip().split()
+        nvtk.append(int(elements[0]))
+        t.append(float(elements[1]))
+        dt.append(float(elements[2]))
+        nsteps.append(int(elements[3]))
+        file_type.append(elements[4])
+        endianess.append(elements[5])
+        quantity_names.append(elements[6:])
+
+    return (np.array(nvtk),
+            np.array(t),
+            np.array(dt),
+            np.array(nsteps),
+            file_type,
+            endianess,
+            quantity_names)
 
 if __name__ == "__main__":
     pluto_vtk =  '/home/ema/simulazioni/sims_pluto/I90/newtransp-rho20/out/data.0025.vtk'
