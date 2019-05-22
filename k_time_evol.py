f819761325794f9198655b4c965298e967dfae97
Momo <emabr90@gmail.com>
2019-05-22 20:24:23 +0200

    Fix script for plotting k time evolution and made script for getting current table

--- k_time_evol.py
+++ k_time_evol.py
@@ -5,9 +5,11 @@ import importlib
 import os
 import pluto_read_frm as prf
 from scipy.constants import mu_0
+import utilities as ut
 
 #plt.close("all")
 importlib.reload(prf)
+importlib.reload(ut)
 # plt.ion()
 
 # <codecell>
@@ -29,7 +31,7 @@ plot_ne_map_each_frame = False
 # pluto_nframes = [-10+20*ii for ii in range(1,15)]
 # pluto_nframes = [24, 50, 74, 100, 120, 150]
 # pluto_nframes = [24, 50, 74, 100, 120, 132, 150]
-pluto_nframes = list(range(300))
+pluto_nframes = list(range(321))
 # z position of z-const lines (in cm)
 # Z lines settings, z lines always start from 0
 N_z_lines = 30
@@ -99,12 +101,14 @@ Dk = np.zeros(k.shape)
 for ii in range(k.shape[1]):
     Dk[:,ii] = k[0,ii] - k[:,ii]
 
+t_I, I = ut.get_currtab(sim)
+
 # <codecell> Plots
 # Countour of Delta k
 #fig, ax = plt.subplots(nrows=2, sharex=True)
-    
+
 gs = gridspec.GridSpec(2, 2,
-                       width_ratios=[25, 1],
+                       width_ratios=[30, 1],
                        height_ratios=[1, 1]
                        )
 
@@ -115,26 +119,32 @@ ax_k = plt.subplot(gs[1,0])
 
 tt, rr = np.meshgrid(times, r_c)
 lev = np.linspace(-1.e-10,250.,11)
-mp = ax_Dk.contourf(tt, rr, Dk, lev, cmap='hot')
+mp = ax_Dk.contourf(tt, rr*1e6, Dk, lev, cmap='hot')
 
 #for ii in range(len(r_c)):
 #    ax[0].axhline(y=r_c[ii], c='k')
 
 ax_k.set_xlabel('Time (ns)')
-ax_Dk.set_ylabel('r ()')
+ax_Dk.set_ylabel('$r$ (μm)')
 # ax.set_ylim([0.,r_cap*1e+2])
-fig.colorbar(mp, cax=ax_Dk_color)
+fig.colorbar(mp, cax=ax_Dk_color, label=r'$\langle g(0) \rangle - \langle g(r) \rangle$ (T/m)')
 
-ax_k.plot(times, k[0,:], '-', label=r'$r\rightarrow 0$')
-ax_k.plot(times, k[-1,:], '-', label='$r=R$')  # QUESTO NON E' ANCHE PROP. ALLA CORRENTE, PERCHE' USO UN CAMPO INTEGRATO E DIVISO PER L
-ax_k.set_ylabel(r'$\frac{<B(r)>}{L}$')
-ax_k.legend()
+ax_k.plot(times, k[0,:], '-', label=r'$\langle g \rangle (0)$')
+ax_k.plot(times, k[-1,:], '-', label=r'$\langle g \rangle (R)$')  # QUESTO NON E' ANCHE PROP. ALLA CORRENTE, PERCHE' USO UN CAMPO INTEGRATO E DIVISO PER L
+ax_k.set_ylabel(r'$\langle g \rangle$'+' (T/m)')
 
-#ax_I = ax_k.twinx()
-#ax_I.set_ylim(0., 2*np.pi*r_cap**2/mu_0*ax_k.get_ylim()[1])
-#ax_I.set_ylim(2*np.pi*r_cap**2/mu_0*np.array(ax_k.get_ylim()))
+ax_I = ax_k.twinx()
+ax_I.plot(t_I*1e9, I, '--', color='k', label='current')
+ax_I.set_ylabel('Current (A)')
+ax_I.set_ylim(0., 250.)
+# ax_I.set_ylim(0., 2*np.pi*r_cap**2/mu_0*ax_k.get_ylim()[1])
+# ax_I.set_ylim(2*np.pi*r_cap**2/mu_0*np.array(ax_k.get_ylim()))
+fig.legend(loc=(0.6,0.31))
+
+
 
 # Set lims to have common lims
+ax_k.grid()
 ax_k.set_xlim(ax_Dk.get_xlim())
 ax_k.set_ylim([0,800])  # bottom=0
 
---
+++ utilities.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+def get_currtab(sim_path, current_finame = 'current_table.dat'):
+    '''
+    Returns time and current (as two separate 1D vectors of same size) of the current table
+    '''
+    curr_fi = os.path.join(os.path.expandvars(sim_path),current_finame)
+    curr = np.loadtxt(curr_fi)
+    return curr[:,0], curr[:,1]  # curr[:,0] is the time in s, curr[:,1] is the current in A.
