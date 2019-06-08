ee2c46f09dfd303d07dac49bb6f6a4a13196366a
Momo <emabr90@gmail.com>
2019-06-08 19:42:55 +0200

    Fix setting system for ne_ztmap

--- ne_ztmap.py
+++ ne_ztmap.py
@@ -16,30 +16,38 @@ importlib.reload(apl)
 # plt.ion()
 
 # <codecell>
-# Options
-# sim = '/home/ema/simulazioni/sims_pluto/dens_real/1e5Pa'
-# sim = '/home/ema/simulazioni/sims_pluto/dens_real/1.3e5Pa'
-# sim = '/home/ema/simulazioni/sims_pluto/dens_real/1.3e5Pa-rhounif-I90-3.2cm'
-# sim = '/home/ema/simulazioni/sims_pluto/dens_real/1.3e5Pa-1.2cm'
-# measurement = 'file:///home/ema/Dottorato/dati_sperimentali_e_calcoli/misure_ne_da_confrontare/misure_capillare_1cmL-1mmD/stessa_scala_temporale/ne_I245_t50-1450ns_z0-10mm_cmapjet0-15e16_CUT.png'
-# measurement = '/home/ema/Dottorato/dati_sperimentali_e_calcoli/misure_ne_da_confrontare/misure_capillare_1cmL-1mmD/stessa_scala_temporale/ne_I245_t50-1450ns_z0-10mm_cmapjet0-15e16_CUT_rotated.png'
+# Settings
 
-# ---
-# sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho8e-8-I90-3.2cmL-1mmD'
-# sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho2.53e-7-I90-3.2cmL-1mmD-r60-NTOT8'
-sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho3.5e-7-I90-3.2cmL-1mmD-r60-NTOT8-diffRecPeriod8'
-# sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho4.5e-7-I90-3.2cmL-1mmD-r60-NTOT16-diffRecPeriod8'
-# sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho8e-7-I90-3.2cmL-1mmD'
-# sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho2.53e-7-I235-3.2cmL-1mmD-r15'
-# measurement = '/home/ema/Dottorato/dati_sperimentali_e_calcoli/misure_ne_da_confrontare/misure_capillare_3cmL-1mmD/stessa_scala_temporale/densità_elettronica/ne_I90_t150-1250ns_z0-30mm_cmapjet0-15e16_CUT.png'
-measurement = '/home/ema/Dottorato/dati_sperimentali_e_calcoli/misure_ne_da_confrontare/misure_capillare_3cmL-1mmD/stessa_scala_temporale/densità_elettronica/ne_I90_t150-1250ns_z0-30mm_cmapjet0-15e16_CUT_rotated.png'
-# ---
+# Allowed choices are '245A-1cm' and '90A-3cm'
+measure_choice = '245A-1cm'
+average_ne = 'integral'  # 'max','integral'
+# This is just for the title label
+rho0 = 3.5e-7   # Initial mass density imposed in simulation
 
-# The frames of pluto which I want to see (it must be a list of integers)
-pluto_nframes = list(range(0,151,5))
-# Capillary radius
-average_ne = 'max'  # 'max','integral'
-reflect_lowz = True
+if measure_choice=='245A-1cm':
+    # ----
+    # sim = '/home/ema/simulazioni/sims_pluto/dens_real/1e5Pa'
+    # sim = '/home/ema/simulazioni/sims_pluto/dens_real/1e4Pa-06012019/'
+    # sim = '/home/ema/simulazioni/sims_pluto/dens_real/1.3e5Pa'
+    sim = '/home/ema/simulazioni/sims_pluto/dens_real/1.3e5Pa-1.2cm'
+    measurement = '/home/ema/Dottorato/dati_sperimentali_e_calcoli/misure_ne_da_confrontare/misure_capillare_1cmL-1mmD/stessa_scala_temporale/ne_I245_t50-1450ns_z0-10mm_cmapjet0-15e16_CUT_rotated.png'
+    pluto_nframes =list(range(0,261,5))
+    l_cap = 1e-2
+elif measure_choice=='90A-3cm':
+    # ---
+    # sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho8e-8-I90-3.2cmL-1mmD'
+    # sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho2.53e-7-I90-3.2cmL-1mmD-r60-NTOT8'
+    sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho3.5e-7-I90-3.2cmL-1mmD-r60-NTOT8-diffRecPeriod8-fast'
+    # sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho4.5e-7-I90-3.2cmL-1mmD-r60-NTOT16-diffRecPeriod8'
+    # sim = '/home/ema/simulazioni/sims_pluto/perTesi/rho8e-7-I90-3.2cmL-1mmD'
+    measurement = '/home/ema/Dottorato/dati_sperimentali_e_calcoli/misure_ne_da_confrontare/misure_capillare_3cmL-1mmD/stessa_scala_temporale/densità_elettronica/ne_I90_t150-1250ns_z0-30mm_cmapjet0-15e16_CUT_rotated.png'
+    # ---
+    # The frames of pluto which I want to see (it must be a list of integers)
+    pluto_nframes =list(range(0,261,5))
+    # Capillary radius (m)
+    l_cap = 3e-2
+else:
+    raise ValueError('Unrecognized measure choice')
 
 #%% Get ne, z-profile
 z_cc, z, ne_avg_r, t = apl.ne_avg_over_r(sim, pluto_nframes, average_ne,
@@ -51,10 +59,9 @@ ne_avg_r = np.array(ne_avg_r)
 # on the z grid)
 ne_avg_r_cc = 0.5*(ne_avg_r[1:,:]+ne_avg_r[:-1,:])
 
-if reflect_lowz:
-    # Maybe the code would work even without flipping, but I do so, to make if more robust
-    z = np.concatenate((np.flip(-z, axis=0), z[1:]))
-    ne_avg_r_cc = np.concatenate((np.flip(ne_avg_r_cc, axis=1), ne_avg_r_cc), axis=1)
+# Maybe the code would work even without flipping, but I do so, to make if more robust
+z = np.concatenate((np.flip(-z, axis=0), z[1:]))
+ne_avg_r_cc = np.concatenate((np.flip(ne_avg_r_cc, axis=1), ne_avg_r_cc), axis=1)
 
 tt,zz = np.meshgrid(t,z)
 
@@ -71,7 +78,7 @@ ax_cb = plt.subplot(gs[:,1])
 filippi = mpimg.imread(measurement)
 # fig, ax = plt.subplots(ncols=2)
 ax_meas.imshow(filippi,
-             extent=[150.,1250., -1.5, 1.5],  # extent=[left,right,bottom,top],
+             extent=[150., 1250., -l_cap/2*1e2, +l_cap/2*1e2],  # extent=[left,right,bottom,top],
              aspect='auto',
              origin='upper'
              )
@@ -81,18 +88,22 @@ mp = ax_sim.pcolormesh(tt, zz, ne_avg_r_cc.T,
                    vmin=0.0)
 
 # ax_sim.invert_yaxis()
-ax_sim.set_ylim([-1.5, 1.5])
+ax_sim.set_ylim([-l_cap/2*1e2, l_cap/2*1e2])
 ax_sim.set_xlim([150., 1250])
-yticks = [-1.5, 0., 1.5]
+yticks = [-l_cap/2*1e2, 0., l_cap/2*1e2]
 ax_meas.set_yticks(yticks)
 ax_sim.set_yticks(yticks)
 # ax_meas.set_xticks(ax_sim.get_xticks())
 # ax_meas.set_xticklabels([])
 
 ax_sim.set_xlabel('Time (s)')
-ax_sim.set_ylabel('z (mm)')
-ax_meas.set_ylabel('z (mm)')
-ax_sim.set_title('Simulation')
+ax_sim.set_ylabel('z (cm)')
+ax_meas.set_ylabel('z (cm)')
+if average_ne == 'max':
+    title = 'Simulation, '+r'$\mathrm{max}_r (n_e) $, '+r'$\rho_0= {:g}'.format(rho0/1e-7)+r'\cdot 10^{-7}$ g/cm³'
+elif average_ne == 'integral':
+    title = 'Simulation, '+r'$\langle n_e \rangle_r$, '+r'$\rho_0= {:g}'.format(rho0/1e-7)+r'\cdot 10^{-7}$ g/cm³'
+ax_sim.set_title(title)
 ax_meas.set_title('Measurement')
 
 cb = fig.colorbar(mp, cax = ax_cb, label='Electron density ($\mathrm{cm}^{-3}$)')
