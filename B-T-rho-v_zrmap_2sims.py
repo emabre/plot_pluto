50f5eca679129220de0d43e15d822f8d6ce87c24
Momo <emabr90@gmail.com>
2019-06-18 20:33:15 +0200

    Add quiver to B,T,rho,v plot

--- B-T-rho-v_zrmap_2sims.py
+++ B-T-rho-v_zrmap_2sims.py
@@ -31,6 +31,12 @@ r_cap = 0.5e-3  # m
 dz_cap = 1.e-3  # m
 rmax = 1e-3  # m
 zmax = 1.2e-2  # m
+# ---- For quiver plot ----
+# This does not influence the physical correctness of the arrows plot, just the way it looks
+quiv_scale = 8e4
+# Lenght of the arrow appearing in the arrow legend ("arrow legend" is "quiver plot key" in matplolib)
+# Also this parameterv does not influence the physical correctness of the arrows plot, just the way it looks
+key_length = 1e4
 
 #%% Load the data
 pluto_dir = os.path.join(os.path.expandvars(sim), 'out')
@@ -50,38 +56,62 @@ r_cc = 0.5*(r[1:]+r[:-1])
 z_cc = 0.5*(z[1:]+z[:-1])
 
 #%% Plotting
-gs = gridspec.GridSpec(6, 2,
-                       width_ratios=[30, 2],
+gs = gridspec.GridSpec(6, 3,
+                       width_ratios=[30, 2, 2],
                        height_ratios=6*[1],
                        hspace = 0
                        )
 
 fig = plt.figure(figsize=(5.5,4.5))
-ax = [[],[]]; ax_cb = [[],[]]
+ax = [[],[]]; ax_cb = [[],[],[]]
 ax[0] = plt.subplot(gs[:3,0])
 ax[1] = plt.subplot(gs[3:,0])
 ax_cb[0] = plt.subplot(gs[:2,1])
 ax_cb[1] = plt.subplot(gs[4:,1])
+ax_cb[2] = plt.subplot(gs[:2,2])
 
-mp_T = ax[0].contourf(z_cc*1e2, r_cc*1e6,
-                    rho.T,
-                     # levels = np.linspace(Tmin,Tmax,101),
-                     # levels = 100,
-                     # levels=np.linspace(Tmin, Tmax, 150),
-                     # locator=ticker.LogLocator(),
-                     # norm = colors.LogNorm(vmin=vmin, vmax=vmax),
-                     # vmin = Tmin,
-                     # vmax = Tmin,
-                     # cmap = 'hot',
-                     cmap = 'pink',
-                     )
+
+mp_rho = ax[0].contourf(z_cc*1e2, r_cc*1e6,
+                        rho.T,
+                         # levels = np.linspace(Tmin,Tmax,101),
+                         # levels = 100,
+                         # levels=np.linspace(Tmin, Tmax, 150),
+                         # locator=ticker.LogLocator(),
+                         # norm = colors.LogNorm(vmin=vmin, vmax=vmax),
+                         # vmin = Tmin,
+                         # vmax = Tmin,
+                         # cmap = 'hot',
+                         cmap = 'pink',
+                         # alpha=0.5
+                         )
+
+arr_color='cyan'
+step_r = 10
+step_z = 2
+Q = ax[0].quiver(z_cc[::step_z]*1e2, r_cc[::step_r]*1e6,
+                 vz.T[::step_r,::step_z],
+                 vr.T[::step_r,::step_z],
+                 scale_units='width',
+                 scale=quiv_scale,
+                 headwidth=3.8, width=0.005,
+                 color=arr_color,
+                 # zorder = 20,
+                 )
+
+qk = ax[0].quiverkey(Q, 0.9, 0.25,
+                   key_length,
+                   'Bulk \n velocity,\n{:2.0e} cm/s'.format(key_length),
+                   labelpos='N',
+                   coordinates='figure',
+                   color='k'
+                   )
 
 # Tmin = 2.5e3
 # Tmax = 80e3
 # for ss in (0,1):
 #     T[ss][T[ss]<2.5e3] = 2.5e3
 # for ss in (0,1):
-mp_rho = ax[1].contourf(z_cc*1e2, r_cc*1e6,
+mp_T = ax[1].contourf(z_cc*1e2, r_cc*1e6,
                      T.T,
                      # levels = np.linspace(Tmin,Tmax,101),
                      # levels = 100,
@@ -93,16 +123,17 @@ mp_rho = ax[1].contourf(z_cc*1e2, r_cc*1e6,
                      # cmap = 'hot',
                      cmap = 'gist_heat',
                      )
-    # # Plot iso-I. Note that in cylindrical coordinates, their density does not represent current density (even though correlated),
-    # # because the current density is not divergent free in cylindrical symmetry with the cartesia definition of divergence ((Dz,Dr)).
-    # mpj = ax[ss].contour(z_cc[ss]*1e2, r_cc[ss]*1e6,
-    #                      (B[ss]*r_cc[ss]*2*np.pi/mu_0).T,  # from Ampere's law, I=B*r*2*pi/mu_0
-    #                      levels = 7,
-    #                      colors='cyan',
-    #                      linewidths = 1.,
-    #                     )
+# Plot iso-I. Note that in cylindrical coordinates, their density does not represent current density (even though correlated),
+# because the current density is not divergent free in cylindrical symmetry with the cartesia definition of divergence ((Dz,Dr)).
+mp_B = ax[1].contour(z_cc*1e2, r_cc*1e6,
+                     B.T,  # from Ampere's law, I=B*r*2*pi/mu_0
+                     levels = 7,
+                     cmap = 'ocean',
+                     # colors='cyan',
+                     linewidths = 1.,
+                    )
 
-ax[0].set_ylim(0.01, rmax*1e6)
+ax[0].set_ylim(rmax*1e6*5e-3, rmax*1e6)
 ax[1].set_ylim(rmax*1e6, 0.01)
 for ii in (0,1):
     ax[ii].set_xlim(0.01, zmax*1e2)
@@ -112,16 +143,17 @@ ax[1].set_xlabel('z (cm)')
 ax[0].set_ylabel('r (μm)')
 ax[1].set_ylabel('r (μm)')
 
-cbar_T = fig.colorbar(mp_T, cax = ax_cb[0],
-                    label='Temperature (K)',
-                    # ticks = [2500, 2e4, 4e4, 6e4, 8e4],
-                    # ticks = [2500, 1e4, 2e4, 3e4, 4e4],
-                    )
-cbar_rho = fig.colorbar(mp_rho, cax = ax_cb[1],
-                    label='Mass density (g/cm3)',
-                    # ticks = [2500, 2e4, 4e4, 6e4, 8e4],
-                    # ticks = [2500, 1e4, 2e4, 3e4, 4e4],
-                    )
+cbar_T = fig.colorbar(mp_T, cax = ax_cb[1],
+                      label='Temperature (K)',
+                      # ticks = [2500, 2e4, 4e4, 6e4, 8e4],
+                      # ticks = [2500, 1e4, 2e4, 3e4, 4e4],
+                      )
+
+cbar_rho = fig.colorbar(mp_rho, cax = ax_cb[0],
+                        label='Mass density (g/cm3)',
+                        # ticks = [2500, 2e4, 4e4, 6e4, 8e4],
+                        # ticks = [2500, 1e4, 2e4, 3e4, 4e4],
+                        )
 # cbar.set_ticklabels(['$<10^8$', '$10^{10}$', '$10^{12}$', '$10^{14}$', '$10^{16}$', '$10^{18}$'])
 
 # ----
@@ -131,9 +163,9 @@ electrode_zr_ll = ((l_cap-dz_cap)*1e2, r_cap*1e6)
 wall_zr_ll = (0, r_cap*1e6)
 
 electrodes = [Rectangle((electrode_zr_ll[0], electrode_zr_ll[1]), dz_cap*1e2, r.max()*1e6,
-                        fill=True, facecolor='#a3552c', edgecolor='k', zorder=10 ) for ss in (0,1)]
+                        fill=True, facecolor='#a3552c', edgecolor='k', zorder=3 ) for ss in (0,1)]
 walls = [Rectangle((wall_zr_ll[0], wall_zr_ll[1]), (l_cap-dz_cap)*1e2, r.max()*1e6,
-                        fill=True, facecolor='gray', edgecolor='k', zorder=10 ) for ss in (0,1)]
+                        fill=True, facecolor='gray', edgecolor='k', zorder=3 ) for ss in (0,1)]
 for ss in (0,1):
     ax[ss].add_patch(electrodes[ss])
     ax[ss].add_patch(walls[ss])
